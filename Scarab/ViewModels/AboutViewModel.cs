708ec9fb4dc4c0d33bccdd3c705ed6b92b338487
huaisha1224 <sam.hxq@gmail.com>
2025-09-05 18:13:18 +0800

    AboutView

--- Scarab/ViewModels/AboutViewModel.cs
+++ Scarab/ViewModels/AboutViewModel.cs
@@ -10,10 +10,25 @@ public class AboutViewModel : ViewModelBase
 
     public ReactiveCommand<Unit, Unit> Download { get; set; } = ReactiveCommand.Create(_Download);
     public ReactiveCommand<Unit, Unit> OpenLogs { get; set; } = ReactiveCommand.Create(_OpenLogs);
+    public ReactiveCommand<Unit, Unit> OpenSource { get; set; } = ReactiveCommand.Create(_OpenSource);
 
-    public string Version { get; } = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+    public string Version { get; } = GetVersion();
+    public string FileVersion { get; } = GetFileVersion();
 
     public static string OSString => $"{OS} {Environment.OSVersion.Version}";
+
+    private static string GetVersion()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        return assembly.GetName().Version?.ToString() ?? "Unknown";
+    }
+
+    private static string GetFileVersion()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var fileVersionAttr = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+        return fileVersionAttr?.Version ?? "Unknown";
+    }
 
     [SuppressMessage("ReSharper", "ConvertIfStatementToReturnStatement")]
     private static string OS
@@ -39,8 +54,14 @@ public class AboutViewModel : ViewModelBase
     {
         Process.Start(new ProcessStartInfo("https://hs2049.cn") { UseShellExecute = true });
     }
+
     private static void _OpenLogs()
     {
         Process.Start(new ProcessStartInfo(Settings.GetOrCreateDirPath()) { UseShellExecute = true });
+    }
+
+    private static void _OpenSource()
+    {
+        Process.Start(new ProcessStartInfo("https://github.com/huaisha1224/HollowKnightMODManager") { UseShellExecute = true });
     }
 }
