4ffbddbd205c00a0f3836e16234c5c428617b46c
huaisha1224 <sam.hxq@gmail.com>
2025-09-13 22:04:50 +0800

    update

--- Scarab/ViewLocator.cs
+++ Scarab/ViewLocator.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Avalonia.Controls.Templates;
+using Scarab.Views;
 
 namespace Scarab;
 
@@ -8,6 +9,18 @@ public class ViewLocator : IDataTemplate
     public Control Build(object? data)
     {
         Debug.Assert(data != null, nameof(data) + " != null");
+        
+        // 特殊处理HelpViewModel和AboutViewModel
+        if (data is Scarab.ViewModels.HelpViewModel)
+        {
+            return new HelpView { DataContext = data };
+        }
+        
+        if (data is Scarab.ViewModels.AboutViewModel)
+        {
+            return new AboutView { DataContext = data };
+        }
+        
         string? name = data.GetType().FullName?.Replace("ViewModel", "View");
 
         if (string.IsNullOrEmpty(name))
@@ -27,6 +40,6 @@ public class ViewLocator : IDataTemplate
 
     public bool Match(object? data)
     {
-        return data is ViewModelBase;
+        return data is ViewModelBase || data is Scarab.ViewModels.HelpViewModel || data is Scarab.ViewModels.AboutViewModel;
     }
 }
